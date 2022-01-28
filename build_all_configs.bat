73323e3782fae46ac4147bc50242be4be0a0a1ba
anthonyj <anthony.johnson@flexsim.com>
2022-01-28 10:06:08 -0500

    More configuration changes
    - Added copyheaders.bat to copy headers from the flexsim directory
    - Adjustments to the projects for proper output directory configuration

--- build_all_configs.bat
+++ build_all_configs.bat
@@ -1,9 +1,11 @@
 @echo off
 
-set "VSCMD_START_DIR=%CD%"
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+set msbuildpath=%1
+if "%msbuildpath%"=="" (
+	set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
+)
 
-msbuild FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_10 /t:Rebuild
-msbuild FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_9 /t:Rebuild
-msbuild FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_8 /t:Rebuild
-msbuild FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_7 /t:Rebuild
+%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_10 /t:Rebuild
+%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_9 /t:Rebuild
+%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_8 /t:Rebuild
+%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_7 /t:Rebuild
