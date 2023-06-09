7648ae37e87af5a90687ad91dec432fc0b0470d1
Matthew Gillespie <matthew.gillespie@flexsim.com>
2023-06-09 13:43:23 -0600

    Add 3.11 to the build all configs script

--- build_all_configs.bat
+++ build_all_configs.bat
@@ -5,6 +5,7 @@ if "%msbuildpath%"=="" (
 	set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
 )
 
+%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_11 /t:Rebuild
 %msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_10 /t:Rebuild
 %msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_9 /t:Rebuild
 %msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_8 /t:Rebuild
