1a48c9dda78af5828b1e524fb3f8a25c55817362
Anthony Johnson <anthony.johnson@autodesk.com>
2024-03-14 14:53:48 -0600

    Took python 3.7 and python 3.8 out of build/install
    These versions have reached end of life, so they should no longer
    be supported.

--- build_all_configs.bat
+++ build_all_configs.bat
@@ -2,12 +2,10 @@
 
 set msbuildpath=%1
 if "%msbuildpath%"=="" (
-	set msbuildpath="C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin\MSBuild.exe"
+	set msbuildpath="C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe"
 )
 
 %msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_11 /t:Rebuild
 %msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_10 /t:Rebuild
 %msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_9 /t:Rebuild
-%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_8 /t:Rebuild
-%msbuildpath% FlexSimPy.sln /p:Platform=x64;Configuration=Rel_3_7 /t:Rebuild
 %msbuildpath% PyConnector\PyConnectorInstaller.sln /p:Configuration=Release /t:Rebuild
